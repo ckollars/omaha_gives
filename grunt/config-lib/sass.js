36d448174e36e86c9ec863dacbec9612d7afe4b6
Christopher Kollars <chris@christopherkollars.com>
2015-02-09 18:12:49 -0600

    Added sass distribution settings.

--- grunt/config-lib/sass.js
+++ grunt/config-lib/sass.js
@@ -7,5 +7,15 @@ module.exports = {
     files: {
       '<%= pkg.config.dev %>/css/main.css': '<%= pkg.config.src %>/scss/main.scss'
     }
+  },
+  dist: {
+    options: {
+      style: 'expanded',
+      debugInfo: false,
+      sourcemap: 'none'
+    },
+    files: {
+      '<%= pkg.config.dist %>/css/main.css': '<%= pkg.config.src %>/scss/main.scss'
+    }
   }
 };

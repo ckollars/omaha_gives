602c1af39eceb15ebd74e8effdd6aaeea61dddc5
Christopher Kollars <chris@christopherkollars.com>
2015-02-16 14:53:21 -0600

    Added user nav click functionality.

--- _source/js/site.js
+++ _source/js/site.js
@@ -1,10 +1,16 @@
 var navButton = $('.btn--mobile-menu'),
+  signupButton = $('.btn--signup'),
   navigation = $('#main-nav');
 
 $(function () {
 
   navButton.on('click', function () {
     navigation.toggleClass('show-menu');
+  });
+
+  signupButton.on('click', function (e) {
+    e.preventDefault();
+    $('#user-nav').toggleClass('menu-signup--active');
   });
 
 });
